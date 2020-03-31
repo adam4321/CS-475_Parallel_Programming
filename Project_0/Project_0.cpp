ddf06362e901f33215a3a33515dbe1ed0ec6878c
Adam Wright <adam@adamjwright.com>
2020-03-30 21:34:51 -0700

    add gitignore

--- Project_0/Project_0.cpp
+++ Project_0/Project_0.cpp
@@ -1,57 +1,60 @@
-
+/******************************************************************************
+**
+**
+**
+******************************************************************************/
 
 #include <omp.h>
 #include <stdio.h>
 #include <math.h>
 
-#define NUMT	         4
-#define SIZE       	??	// you decide
-#define NUMTRIES        ??	// you decide
+#define NUMT	        1   // Thread count
+#define SIZE       	16384	// Array size
+#define NUMTRIES      100	// Number of iterations
 
 float A[SIZE];
 float B[SIZE];
 float C[SIZE];
 
-int
-main( )
+int main()
 {
-#ifndef _OPENMP
-        fprintf( stderr, "OpenMP is not supported here -- sorry.\n" );
+    #ifndef _OPENMP
+        fprintf(stderr, "OpenMP is not supported here -- sorry.\n");
         return 1;
-#endif
+    #endif
 
-	// inialize the arrays:
-	for( int i = 0; i < SIZE; i++ )
-	{
-		A[ i ] = 1.;
-		B[ i ] = 2.;
-	}
+    // Inialize the arrays
+    for (int i = 0; i < SIZE; i++)
+    {
+        A[i] = 1.;
+        B[i] = 2.;
+    }
 
-        omp_set_num_threads( NUMT );
-        fprintf( stderr, "Using %d threads\n", NUMT );
+    omp_set_num_threads(NUMT);
+    fprintf(stderr, "Using %d threads\n", NUMT);
 
-        double maxMegaMults = 0.;
+    double maxMegaMults = 0.;
 
-        for( int t = 0; t < NUMTRIES; t++ )
+    for (int t = 0; t < NUMTRIES; t++)
+    {
+        double time0 = omp_get_wtime();
+
+        #pragma omp parallel for
+        for (int i = 0; i < SIZE; i++)
         {
-                double time0 = omp_get_wtime( );
-
-                #pragma omp parallel for
-                for( int i = 0; i < SIZE; i++ )
-                {
-                        C[i] = A[i] * B[i];
-                }
-
-                double time1 = omp_get_wtime( );
-                double megaMults = (double)SIZE/(time1-time0)/1000000.;
-                if( megaMults > maxMegaMults )
-                        maxMegaMults = megaMults;
+            C[i] = A[i] * B[i];
         }
 
-        printf( "Peak Performance = %8.2lf MegaMults/Sec\n", maxMegaMults );
+        double time1 = omp_get_wtime();
+        double megaMults = (double)SIZE / (time1 - time0) / 1000000.;
+        if (megaMults > maxMegaMults)
+            maxMegaMults = megaMults;
+    }
 
-	// note: %lf stands for "long float", which is how printf prints a "double"
-	//        %d stands for "decimal integer", not "double"
+    printf("Peak Performance = %8.2lf MegaMults/Sec\n", maxMegaMults);
 
-        return 0;
+    /* NOTE: %lf stands for "long float", which is how printf prints a "double"
+            %d stands for "decimal integer", not "double" */
+
+    return 0;
 }
