8dbc14dc8519dc04e8fe5f0fdb26170eb3981be7
Adam Wright <adam@adamjwright.com>
2020-04-17 05:04:18 -0700

    working version

--- Project_2/Project_2.cpp
+++ Project_2/Project_2.cpp
@@ -3,7 +3,7 @@
 **  Email:        wrighada@oregonstate.edu
 **  Date:         4-16-2020
 **  Description:  Program 2 for OSU cs-475 Parallel Programming. The program
-**                uses OpenMP and is a test of the performance of 
+**                uses OpenMP and is a test of the performance of
 ******************************************************************************/
 
 #include <stdio.h>
@@ -19,9 +19,9 @@
 #define NUMT		1
 #endif
 
-// Setting the number of trials in the simulation:
+// Setting the number of nodes per side in the simulation:
 #ifndef NUMNODES
-#define NUMNODES	100
+#define NUMNODES	1000
 #endif
 
 // How many tries to discover the maximum performance:
@@ -32,64 +32,83 @@
 // Define the exponent of the n-sphere
 #define N 4
 
-// Define the 
+// Define the dimensions of the superquadratic
 #define XMIN     -1.
 #define XMAX      1.
 #define YMIN     -1.
 #define YMAX      1.
 
 // Provided function for calculating the height at a given point (iu, iv)
-float Height( int, int );
+float Height(int, int);
 
 
-int main( int argc, char *argv[ ] )
+int main(int argc, char* argv[])
 {
-    #ifndef _OPENMP
-		fprintf(stderr, "No OpenMP support!\n");
-		return 1;
-	#endif
+#ifndef _OPENMP
+    fprintf(stderr, "No OpenMP support!\n");
+    return 1;
+#endif
 
+    // Set the number of threads to use in the for-loop
+    omp_set_num_threads(NUMT);
 
-	omp_set_num_threads(NUMT);	// set the number of threads to use in the for-loop:`
+    // Get ready to record the maximum performance
+    float maxPerformance = 0.;      // Must be declared outside the NUMTRIES loop
+    float volume;              // must be declared outside the NUMTRIES loop
 
-	// get ready to record the maximum performance and the probability:
-	float maxPerformance = 0.;      // must be declared outside the NUMTRIES loop
-	float currentProb = 0.;              // must be declared outside the NUMTRIES loop
+    // The area of a single full-sized tile:
+    float fullTileArea = (((XMAX - XMIN) / (float)(NUMNODES - 1)) *
+        ((YMAX - YMIN) / (float)(NUMNODES - 1)));
 
-	// The area of a single full-sized tile:
-	float fullTileArea = (  ( ( XMAX - XMIN )/(float)(NUMNODES-1) )  *
-				( ( YMAX - YMIN )/(float)(NUMNODES-1) )  );
-
-	// sum up the weighted heights into the variable "volume"
-	// using an OpenMP for loop and a reduction:
-
-	/////////////////////////
-
+    // Sum up the weighted heights into the variable "volume"
+    // using an OpenMP for loop and a reduction:
     for (int i = 0; i < NUMTRIES; i++)
     {
+        volume = 0.;
+
+        // Start the calculation timer
         double time0 = omp_get_wtime();
 
-        #pragma omp parallel for default(none) . . .
-        for( int i = 0; i < NUMNODES*NUMNODES; i++ )
+        #pragma omp parallel for default(none) reduction(+:volume)
+        for (int i = 0; i < NUMNODES * NUMNODES; i++)
         {
             int iu = i % NUMNODES;
             int iv = i / NUMNODES;
-            float z = Height( iu, iv );
+            float z = Height(iu, iv);
 
-            ////////////////////////
+            // Calculate area from current height sample
+            double tile_area = (z * 2) * fullTileArea;
+
+            // Add current value to volume with 1/4 for corners and 1/2 for edges
+            if (iu == 0 || iu == NUMNODES - 1)
+            {
+                tile_area /= 2;
+            }
+            if (iv == 0 || iv == NUMNODES - 1)
+            {
+                tile_area /= 2;
+            }
+
+            // Add to the total volume
+            volume += tile_area;
         }
 
-
+        // Stop timer
         double time1 = omp_get_wtime();
-		double megaHeightsPerSecond = (double)NUMNODES / (time1 - time0) / 1000000.;
+
+        // Calculate the operations / sec of this run
+        double megaHeightsPerSecond = ((double)NUMNODES * (double)NUMNODES)
+            / (time1 - time0) / 1000000.;
 
         // Prevent a divide by 0 infinite result from poluting the data
         if (isfinite(megaHeightsPerSecond))
-        {  
+        {
             if (megaHeightsPerSecond > maxPerformance)
                 maxPerformance = megaHeightsPerSecond;
         }
     }
+
+    printf("Volume: %lf\n", volume);
 
     // Print out the maximum performance value of the trials set 
     printf("%.2f\t", maxPerformance);
@@ -98,16 +117,16 @@ int main( int argc, char *argv[ ] )
 }
 
 
-float Height( int iu, int iv )	// iu,iv = 0 .. NUMNODES-1
+float Height(int iu, int iv)	// iu,iv = 0 .. NUMNODES-1
 {
-	float x = -1.  +  2.*(float)iu /(float)(NUMNODES-1);	// -1. to +1.
-	float y = -1.  +  2.*(float)iv /(float)(NUMNODES-1);	// -1. to +1.
+    float x = -1. + 2. * (float)iu / (float)(NUMNODES - 1);	// -1. to +1.
+    float y = -1. + 2. * (float)iv / (float)(NUMNODES - 1);	// -1. to +1.
 
-	float xn = pow( fabs(x), (double)N );
-	float yn = pow( fabs(y), (double)N );
-	float r = 1. - xn - yn;
-	if( r < 0. )
-	        return 0.;
-	float height = pow( 1. - xn - yn, 1./(float)N );
-	return height;
+    float xn = pow(fabs(x), (double)N);
+    float yn = pow(fabs(y), (double)N);
+    float r = 1. - xn - yn;
+    if (r < 0.)
+        return 0.;
+    float height = pow(1. - xn - yn, 1. / (float)N);
+    return height;
 }
